b51deca831eb32eb7975e138faa8bd5e5a844fad
MuriloBianchi <murilolopes.b@gmail.com>
2024-09-11 19:55:13 -0300

    Resources e Services

---
+++ demo/src/main/java/com/biblioteca/resources/UsuarioResource.java
@@ -0,0 +1,10 @@
+package com.biblioteca.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class UsuarioResource {
+
+}
---
+++ demo/src/main/java/com/biblioteca/services/LivroService.java
@@ -0,0 +1,8 @@
+package com.biblioteca.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+
+}
